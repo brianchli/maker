119fd8a880a2d9cf9c528d9fb42797d9bd90d9f2
Brian Li <brianchunghli@gmail.com>
2026-05-02 11:33:04 +1000

    feat(util): add optional filetype argument and help flag
    Allow filetype to be passed as a CLI argument instead of always
    prompting via gum picker.

--- maker-util/maker-util.sh
+++ maker-util/maker-util.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env sh
 
 COMMAND="$1"
+FILE_T="$2"
 if [[ -z ${COMMAND} ]]; then
   COMMAND=$(gum choose --header "maker endpoint" "create" "models") || exit 0
 fi
 case ${COMMAND} in
   "models") curl maker/models ;;
   "create")
-    FILE_T=$(gum choose --header "filetype" "cmake" "makefile" "readme") || exit 0
+    if [[ -z ${FILE_T} ]]; then
+      FILE_T=$(gum choose --header "filetype" "cmake" "makefile" "readme") || exit 0
+    fi
     CONTENT=$(gum write\
       --cursor.foreground "#04AC45" \
       --prompt.foreground "#04B575" \
@@ -22,6 +25,9 @@ case ${COMMAND} in
       || exit 0 \
       && gum spin --spinner dot --title "Generating ${FILE_T}..." --show-output -- \
     curl maker/create -d "${REQUEST}"
+    ;;
+
+  "-h" | "help") echo "help"
     ;;
   *) echo "error: unknown command"
     ;;
