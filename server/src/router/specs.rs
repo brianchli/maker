8e2a058823063caee9e0d128bae32f017e0388bd
Brian Li <brianchunghli@gmail.com>
2026-05-12 10:47:45 +1000

    refactor(spec): serialise into a json vector

--- server/src/router/specs.rs
+++ server/src/router/specs.rs
@@ -1,10 +1,13 @@
-use std::{fmt::Display, os::unix::ffi::OsStrExt};
+use std::fmt::Display;
 
 use http_body_util::{Full, combinators::BoxBody};
 use hyper::{Method, StatusCode, body::Body};
 use tower::BoxError;
 
-use crate::service::{AppState, Req, Response, error_response};
+use crate::{
+    service::{AppState, Req, Response, error_response},
+    some_or_err,
+};
 
 pub async fn specs_route<B>(state: AppState, req: Req<B>) -> Result<Response, BoxError>
 where
@@ -18,10 +21,15 @@ where
     let mut specifications = vec![];
     while let Some(file) = specs.next_entry().await? {
         if file.path().extension().is_some_and(|s| s == "toml") {
-            specifications.extend_from_slice(file.file_name().as_bytes());
-            specifications.push(b'\n');
+            specifications.push(
+                some_or_err!(
+                    file.file_name().to_string_lossy().strip_suffix(".toml"),
+                    "unable to parse toml specification due to extension"
+                )
+                .to_owned(),
+            )
         }
     }
-    let body = Full::from(specifications);
+    let body = Full::from(serde_json::to_vec(&specifications)?);
     Ok(hyper::Response::new(BoxBody::new(body)))
 }
